9235071f1962578cd7e23d4639733cb085a3e6f5
maxx4 <emanson2004@gmail.com>
2026-09-24 16:57:29 +0300

    Develop round feature

--- src/main/java/org/maximys/colorFloor/manager/GameManager.java
+++ src/main/java/org/maximys/colorFloor/manager/GameManager.java
@@ -8,8 +8,10 @@ import org.maximys.colorFloor.ColorFloor;
 import org.maximys.colorFloor.game.GameState;
 import org.maximys.colorFloor.game.JoinResult;
 import org.maximys.colorFloor.task.CountdownTask;
+import org.maximys.colorFloor.task.RoundTask;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
@@ -20,6 +22,7 @@ public class GameManager {
     private final ColorFloor colorFloor;
     private GameState state = GameState.WAIT_FOR_PLAYERS;
     private BukkitTask countdownTask;
+    private BukkitTask roundTask;
 
     public GameManager(ArenaManager arenaManager, ColorFloor colorFloor) {
         this.arenaManager = arenaManager;
@@ -79,13 +82,13 @@ public class GameManager {
         if (state == GameState.START_SOON && players.size() < playerMin) {
             state = GameState.WAIT_FOR_PLAYERS;
 
-            cancelTimer();
+            cancelTask();
         }
 
         if (players.isEmpty()) {
             state = GameState.WAIT_FOR_PLAYERS;
 
-            cancelTimer();
+            cancelTask();
 
             arenaManager.undoArena();
         }
@@ -106,13 +109,19 @@ public class GameManager {
             state = GameState.WAIT_FOR_PLAYERS;
             arenaManager.undoArena();
 
-            cancelTimer();
+            cancelTask();
         }
     }
 
     public void broadcast(String message) {
         if (!players.isEmpty()) {
-            players.keySet().forEach(player -> Bukkit.getPlayer(player).sendMessage(message));
+            players.keySet()
+                    .forEach(uuid -> {
+                        Player player = Bukkit.getPlayer(uuid);
+                        if (player != null) {
+                            player.sendMessage(message);
+                        }
+                    });
         }
     }
 
@@ -123,12 +132,60 @@ public class GameManager {
 
         broadcast("Game started");
         colorFloor.getLogger().info("Game started");
+
+        roundTask = new RoundTask(colorFloor, arenaManager, this).runTaskTimer(colorFloor, 0, 20L);
     }
 
-    private void cancelTimer() {
+    public void eliminateFallen() {
+        Iterator<Map.Entry<UUID, Location>> iterator = players.entrySet().iterator();
+
+        Optional<Location> center = arenaManager.getArenaCenter();
+
+        if (!center.isPresent()) {
+            return;
+        }
+        while (iterator.hasNext()) {
+            Map.Entry<UUID, Location> entry = iterator.next();
+            Player player = Bukkit.getPlayer(entry.getKey());
+
+            if (player == null) {
+                iterator.remove();
+                continue;
+            }
+
+            if (player.getLocation().getY() < center.get().getY()) {
+                player.teleport(entry.getValue());
+                player.sendMessage("Player " + player.getName() + " has been eliminated");
+                iterator.remove();
+            }
+        }
+    }
+
+    public void finishGame() {
+        state = GameState.FINISHED;
+
+        if (players.size() == 1) {
+            Player player = Bukkit.getPlayer(players.keySet().iterator().next());
+            broadcast("Player " + player.getName() + " win");
+        } else {
+            broadcast("nobody won");
+        }
+        stopGame();
+    }
+
+    public int getPlayersCount() {
+        return players.size();
+    }
+
+    private void cancelTask() {
         if (countdownTask != null) {
             countdownTask.cancel();
             countdownTask = null;
+        }
+
+        if (roundTask != null) {
+            roundTask.cancel();
+            roundTask = null;
         }
     }
 }
